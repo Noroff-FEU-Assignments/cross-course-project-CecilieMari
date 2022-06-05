bf5fd7b7ac292ccfacc9cec8cd12145d6075c696
CecilieMari <95425622+CecilieMari@users.noreply.github.com>
2022-06-05 17:54:57 +0200

    nav bar
    endring av nav bar, in mobil veiw and normal view

---
+++ js/rainydays.js
@@ -0,0 +1,30 @@
+
+/*function myFunction() {
+    document.getElementById("myDropdown") .classList.toggle("show");
+}
+
+window.onclick = function(e) {
+    if (!e.target.matches(".dropbutton")) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains("show")) {
+            myDropdown.classList.remove("show");
+        }
+    }
+} */
+
+function show() {
+    var courses = document.getElementById("courses_id");
+
+    if (courses.style.display == "block") {
+        courses.style.display = "none";
+    } else {
+        courses.style.display = "block";
+    }
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches( ".dropbutton")) {
+        document.getElementById("courses_id") 
+        .style.display = "none";
+    }
+}
